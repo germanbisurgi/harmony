30ad6878311e330ffb2df1a9caa139bc823fdda3
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2020-03-28 20:37:33 +0100

    prevent create null

--- src/scene-system/scene-system.js
+++ src/scene-system/scene-system.js
@@ -1,12 +1,6 @@
 const SceneSystem = function () {
   this.current = null
-  this.cache = []
   this.requested = null
-}
-
-SceneSystem.prototype.add = function (scene) {
-  this.cache.push(scene)
-  return scene
 }
 
 SceneSystem.prototype.switch = function (scene) {
@@ -15,6 +9,9 @@ SceneSystem.prototype.switch = function (scene) {
 
 SceneSystem.prototype.update = function () {
   if (this.requested) {
+    if (this.current) {
+      this.current.created = false
+    }
     this.current = this.requested
     this.requested = null
   }
