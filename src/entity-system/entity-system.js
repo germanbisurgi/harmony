b3839f2798076de7e3f749d37d3bff0316dd6eec
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2020-03-28 22:32:50 +0100

    entities destroy

--- src/entity-system/entity-system.js
+++ src/entity-system/entity-system.js
@@ -1,10 +1,21 @@
 /* global Harmony */
 
 const EntitySystem = function () {
+  this.cache = []
 }
 
-EntitySystem.prototype.add = function (config) {
-  return new Harmony.Entity(config)
+EntitySystem.prototype.add = function () {
+  const entity = new Harmony.Entity()
+  this.cache.push(entity)
+  return entity
+}
+
+EntitySystem.prototype.destroy = function () {
+  for (let i = this.cache.length; i--;) {
+    const entity = this.cache[i]
+    entity.destroy()
+  }
+  this.cache = []
 }
 
 export default EntitySystem
--- src/entity-system/entity.js
+++ src/entity-system/entity.js
@@ -1,12 +1,20 @@
 /* global Harmony */
 
 const Entity = function (config) {
+  this.components = []
   this.addComponent(new Harmony.TransformComponent(config))
 }
 
 Entity.prototype.addComponent = function (component) {
   component.owner = this
   this[component.name] = component
+  this.components.push(component)
+}
+
+Entity.prototype.destroy = function () {
+  this.components.forEach((component) => {
+    component.destroy()
+  })
 }
 
 export default Entity
