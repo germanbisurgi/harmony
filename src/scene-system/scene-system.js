ea62510befe28f0a82216e4c28ac6f7a68a636da
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2020-03-29 16:56:45 +0200

    scene requests methods

--- src/scene-system/scene-system.js
+++ src/scene-system/scene-system.js
@@ -7,14 +7,4 @@ SceneSystem.prototype.switch = function (scene) {
   this.requested = scene
 }
 
-SceneSystem.prototype.update = function () {
-  if (this.requested) {
-    if (this.current) {
-      this.current.created = false
-    }
-    this.current = this.requested
-    this.requested = null
-  }
-}
-
 export default SceneSystem
